a1c588304662e3686ae5e1f73bd1cecc1c32d467
Bharath-Kumar-S <bharathkumarkarthick@gmail.com>
2022-07-26 00:52:25 +0300

    adding basic layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+
+const tasks = [
+  {
+    task: "Meet Ramya",
+    status: true,
+    desc: `Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis`,
+  },
+  {
+    task: "Learn something",
+    status: false,
+    desc: `Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis`,
+  },
+  {
+    task: "Learn React",
+    status: true,
+    desc: `Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis`,
+  },
+  {
+    task: "Learn Angular",
+    status: false,
+    desc: `Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis`,
+  },
+  {
+    task: "Learn Mern",
+    status: true,
+    desc: `Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks dis Tasks
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis dis
+              dis dis dis dis dis dis dis dis dis dis dis`,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Task Tracker
+        <section className="tasks-box">
+          {tasks.map(({desc,task,status}) => (
+            <div className="tasks" style= {{textDecorationLine: status ? "line-through" : "", backgroundColor: status ? "cadetblue": ""}}>
+              {task} <img src={logo} className="remove" alt="logo" />
+              <div className="dis">
+                {desc}
+              </div>
+            </div>
+          ))}
+        </section>
       </header>
     </div>
   );
